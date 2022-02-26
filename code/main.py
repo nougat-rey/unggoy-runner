12d975e2efe0e871c207769a54142db17451f734
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-02-26 00:07:38 -0500

    Display main menu messages and background

--- code/main.py
+++ code/main.py
@@ -2,6 +2,28 @@ import pygame
 from settings import *
 from sys import exit
 
+class MainMenu:
+    def __init__(self):
+
+        #background
+        menu_bg = pygame.image.load('../graphics/background/bg_1.jpeg').convert_alpha()
+        screen.blit(menu_bg, menu_bg.get_rect(center = (450,235)))
+
+        #menu messages
+        self.main_font = pygame.font.Font('../font/halo_outline.ttf', 100)
+        main_msg_surf = self.main_font.render('Halo', True, '#000000')
+        main_msg_rect = main_msg_surf.get_rect(center = (450, 125))
+        self.display_surface = pygame.display.get_surface()
+        self.display_surface.blit(main_msg_surf, main_msg_rect)
+
+        self.sec_font = pygame.font.Font('../font/verdana.ttf', 22)
+        sec_msg_surf = self.sec_font.render('Unggoy Runner', True, '#000000')
+        sec_msg_rect = sec_msg_surf.get_rect(center = (450, 175))
+        self.display_surface = pygame.display.get_surface()
+        self.display_surface.blit(sec_msg_surf, sec_msg_rect)
+        
+
+
 class Game:
     def __init__(self):
 
@@ -16,6 +38,7 @@ class Game:
             self.menu_bg.stop()
         else: #in main menu
             self.menu_bg.play()
+            menu = MainMenu()
 
 if __name__ == '__main__':
 
