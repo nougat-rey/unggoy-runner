3a5b5d23b7d257e1ac99c6c9b9dd3c16aaae3636
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-02-25 23:38:21 -0500

    Play main menu music

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # unggoy-runner
 Play as an unggoy (grunt) from the Halo universe as it tries to evade the flood infection form coming after it! Try to survive as long as possible!
+
+## Acknowledgements
+
+This game uses tracks from the following soundtracks:
+
+1. Halo 2 Original Soundtrack by Martin O'Donnell and Michael Salvatori
+    
+    + Mombasa Suite
--- code/main.py
+++ code/main.py
@@ -2,25 +2,37 @@ import pygame
 from settings import *
 from sys import exit
 
-
 class Game:
     def __init__(self):
 
         #general setup
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Unggoy Runner!')
-        self.clock = pygame.time.Clock()
+        self.game_active = False
 
+        #audio
+        self.menu_bg = pygame.mixer.Sound('../audio/Mombasa_Suite.mp3')
 
     def run(self):
-        
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+        if self.game_active:
+            self.menu_bg.stop()
+        else: #in main menu
+            self.menu_bg.play()
 
 if __name__ == '__main__':
+
+    #pygame setup
     pygame.init()
     game = Game()
-    game.run()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Unggoy Runner!')
+    clock = pygame.time.Clock()
+        
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        
+            game.run()
+            pygame.display.update()
+            clock.tick(60)
+        
