362429b528669b50dc196863aea47e0cc5028fd4
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-03-03 11:51:05 -0500

    Add Level class and implement connection between Menu and Level
    - Moved Sound objects to Game class, to be played by Game class
    - Level Class and Menu class each display own graphics on initialization
    - Level class and Menu class each check for input, space from menu to start level, return from level to go back to menu
    - methods defined in Game class and passed into Level and Menu classes handle going back and forth between menu and level
    - Above methods also handle switching between respective background music

--- code/main.py
+++ code/main.py
@@ -2,68 +2,119 @@ import pygame
 from settings import *
 from sys import exit
 
-class MainMenu:
-    def __init__(self):
+
+
+class Menu:
+    def __init__(self, create_level):
+        
+        #screen
+        self.display_surface = pygame.display.get_surface()
 
         #background
-        menu_bg = pygame.image.load('../graphics/background/bg_1.jpeg').convert_alpha()
-        screen.blit(menu_bg, menu_bg.get_rect(center = (450,235)))
-
+        self.menu_bg = pygame.image.load('../graphics/background/bg_1.jpeg').convert()
+        
         #menu messages
         self.main_font = pygame.font.Font('../font/halo_outline.ttf', 100)
-        main_msg_surf = self.main_font.render('Halo', True, '#000000')
-        main_msg_rect = main_msg_surf.get_rect(center = (450, 125))
-        self.display_surface = pygame.display.get_surface()
-        self.display_surface.blit(main_msg_surf, main_msg_rect)
+        self.main_msg_surf = self.main_font.render('Halo', True, '#000000')
+        self.main_msg_rect = self.main_msg_surf.get_rect(center = (450, 125))
 
         self.sec_font = pygame.font.Font('../font/verdana.ttf', 22)
-        sec_msg_surf = self.sec_font.render('Unggoy Runner', True, '#000000')
-        sec_msg_rect = sec_msg_surf.get_rect(center = (450, 175))
+        self.sec_msg_surf = self.sec_font.render('Unggoy Runner', True, '#000000')
+        self.sec_msg_rect = self.sec_msg_surf.get_rect(center = (450, 175))
+        
+        #display
+        self.display_surface.blit(self.menu_bg, self.menu_bg.get_rect(center = (450,235)))
+        self.display_surface.blit(self.main_msg_surf, self.main_msg_rect)
+        self.display_surface.blit(self.sec_msg_surf, self.sec_msg_rect)
+        
+        #create level from menu
+        self.create_level = create_level
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            #start level
+            self.create_level()
+
+    def run(self):
+        self.input()
+        
+        
+class Level:
+    def __init__(self, create_menu):
+        
+        #screen
         self.display_surface = pygame.display.get_surface()
-        self.display_surface.blit(sec_msg_surf, sec_msg_rect)
+        
+        #background
+        self.menu_bg = pygame.image.load('../graphics/background/bg_2.jpeg').convert_alpha()
+
+        #display
+        self.display_surface.blit(self.menu_bg, self.menu_bg.get_rect(center = (450,235)))
+
+        #create menu from level
+        self.create_menu = create_menu
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            #go back to menu
+            self.create_menu()
+
+    def run(self):
+        self.get_input()
+        
 
 class Game:
-    def __init__(self, menu_bg_music):
-
-        #general setup
-        self.game_active = False
+    def __init__(self):
 
         #audio 
-        self.menu_bg_music = menu_bg_music
-        self.menu_bg_music.play(loops = -1)
-        self.menu_music_playing = True
+        self.menu_music = pygame.mixer.Sound('../audio/Mombasa_Suite.mp3')
+        self.level_music = pygame.mixer.Sound('../audio/Peril.mp3')
+
+        #menu creation
+        self.menu = Menu(self.create_level)
+        self.menu_music.play(loops = -1)
+        self.status = 'menu'
+
+    def create_level(self):
+        self.level = Level(self.create_menu)
+        self.status = 'level'
+        self.menu_music.stop()
+        self.level_music.play(loops = -1)
+    
+    def create_menu(self):
+        self.menu = Menu(self.create_level)
+        self.status = 'menu'
+        self.level_music.stop()
+        self.menu_music.play(loops = -1)
 
     def run(self):
 
-        if self.game_active:
-            self.menu_bg_music.stop()
-            self.menu_music_playing = False
-        else:
-            if self.menu_music_playing == False:
-                self.menu_bg_music.play()
-                self.menu_music_playing = True
-            menu = MainMenu()
+        if self.status == 'menu':
+            self.menu.run()
+        elif self.status == 'level':
+            self.level.run()
 
 if __name__ == '__main__':
 
     #pygame setup
     pygame.init()
+
     #background music has a sample frequency of 44.1kHz, stereo sound
     pygame.mixer.init(frequency = 96000, size=-16, channels=2, buffer=1024) 
-    menu_bg = pygame.mixer.Sound('../audio/Mombasa_Suite.mp3')
-    
-    game = Game(menu_bg)
+
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Unggoy Runner!')
     clock = pygame.time.Clock()
-        
+    game = Game()        
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        
-            game.run()
-            pygame.display.update()
-            clock.tick(60)
-    
+
+        game.run()
+        pygame.display.update()
+        clock.tick(60)
